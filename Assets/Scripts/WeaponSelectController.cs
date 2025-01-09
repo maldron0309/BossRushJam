260d124f43e821d8402fdce776a415354b974bcb
ellpArM <1zhangdar2@gmail.com>
2025-01-08 20:55:33 -0500

    WeaponChoosingSystem

---
+++ Assets/Scripts/WeaponSelectController.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WeaponSelectController : MonoBehaviour
+{
+    int currentslot;
+    WeaponSlot currentweapon;
+    bool[] filled;
+    public Image[] slots;
+    public WeaponsInventory inv;
+    public TMP_Text description;
+    public GameObject weaponbuttons;
+    public WeaponWheelController weaponWheel;
+    public PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filled = new bool[8];
+        currentslot = 0;
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void finish()
+    {
+        bool good = true;
+        for (int i = 0; i < 8; i++)
+        {
+            if(filled[i] == false)
+            {
+                good = false;
+                break;
+            }
+        }
+        if (good)
+        {
+            gameObject.SetActive(false);
+            Time.timeScale = 1f;
+            weaponWheel.gameObject.SetActive(true);
+            weaponWheel.updateWheel();
+            inv.GiveWeapon(player);
+        }
+        
+    }
+
+    public void setActive(int num)
+    {
+        transform.Find("Buttons").transform.GetChild(currentslot).GetComponent<Button>().interactable = true;
+        currentslot = num;
+        Debug.Log("Set active to slot " + num.ToString());
+        transform.Find("Buttons").transform.GetChild(num).GetComponent<Button>().interactable = false;
+    }
+    public void setWeapon(WeaponSlot weapon)
+    {
+        if (currentweapon != null) { weaponbuttons.transform.Find(currentweapon.weaponName).GetComponent<Button>().interactable = true; }
+        currentweapon = weapon;
+        description.text = weapon.weaponDescription;
+        Debug.Log("Set active to slot " + weapon.weaponName);
+        weaponbuttons.transform.Find(weapon.weaponName).GetComponent<Button>().interactable = false;
+    }
+
+    void updateWheel()
+    {
+        
+        for (int i = 0; i < inv.weapons.Length; i++)
+        {
+            slots[i].sprite = inv.weapons[i].weaponImage;
+            slots[i].gameObject.SetActive(true);
+        }
+        for (int i = 0; i < 8; i++)
+        {
+            if (filled[i] == false)
+            {
+                slots[i].gameObject.SetActive(false);
+            }
+
+        }
+    }
+
+    public void clear()
+    {
+        for(int i = 0; i<8; i++)
+        {
+            filled[i] = false;
+        }
+        updateWheel();
+        foreach (Button child in transform.Find("Buttons").transform.GetComponentsInChildren<Button>()) { child.interactable = true; currentslot = 0; }
+        transform.Find("Buttons").transform.GetChild(0).GetChild(0).GetComponent<Button>().interactable = false;
+    }
+
+    public void refreshMenu()
+    {
+        clear();
+        //Write stuff to determine what items the player currntly has unlocked and put onto the shop thing
+    }
+
+    public void swap()
+    {
+        inv.weapons[currentslot] = currentweapon;
+        filled[currentslot] = true;
+        updateWheel();
+    }
+}
